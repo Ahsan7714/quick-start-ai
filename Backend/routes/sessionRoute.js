0f2573fffd5955ca3ea6c4a8d900bca6a005209b
Ahsan7714 <ahsan1617k@gmail.com>
2026-07-28 11:41:55 +0500

    chore: remove bulky API doc block comments from routes

--- Backend/routes/sessionRoute.js
+++ Backend/routes/sessionRoute.js
@@ -5,16 +5,9 @@ const {
     addMessageToSession
 } = require("../controllers/sessionController");
 
-// POST -> Add messages to a session
-// URL -> /api/v1/session/addMessages
-// Description -> Add messages to a session by chatbot widget
 
 router.route("/addMessages").post(addMessageToSession);
 
-// POST -> create a session
-// URL -> /api/v1/session/create
-// Description -> Create a session by chatbot widget
-// Request Body -> username, email, chatbotId
 
 router.route("/create").post(createSession);
 
